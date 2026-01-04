edb1bf4bfe1d1ff84a9d257b26644457da1bad7f
thobbiz <odelolatojumi@gmail.com>
2026-01-04 23:41:17 +0100

    feat: solved longest number of substrings problem

---
+++ longestNoOfSubstring.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        charSet = set()
+        left = 0
+        maxNo = 0
+
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+
+            charSet.add(s[right])
+            maxNo = max(maxNo, right - left + 1)
+
+        return maxNo
