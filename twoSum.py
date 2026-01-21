8107fb1ad6e274014d36445b2d8c017c70332436
thobbiz <odelolatojumi@gmail.com>
2026-01-21 01:29:19 +0100

    feat: solved two sum II and renames original two sum

--- twoSum.py
+++ twoSum.py
@@ -15,19 +15,5 @@ def twoSum(nums, target):
             result.append(index)
             result.append(history[value])
             break
-        history[current] = index
 
     return result
-
-
-def main():
-    nums = [2, 7, 11, 15]
-    target = 18
-
-    # Run the code
-    result = twoSum(nums, target)
-    print(result)
-
-
-if __name__ == "__main__":
-    main()
