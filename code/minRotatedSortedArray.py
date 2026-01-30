5b2b29c4a31f660b29d459af2792f5b5d1285ec7
thobbiz <odelolatojumi@gmail.com>
2026-01-30 21:47:58 +0100

    feat: solved the minimum rotated sorted array problem

---
+++ code/minRotatedSortedArray.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def findMin(self, nums: List[int]) -> int:
+    low = 0
+    high = len(nums) - 1
+
+    while low < high:
+        mid = (high + low) // 2
+        if nums[mid] > nums[high]:
+            low = mid + 1
+        else:
+            high = mid
+    return nums[low]
