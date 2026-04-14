252e056dd7d6ae36a45379a7054bdfdd5f1c810e
thobbiz <odelolatojumi@gmail.com>
2026-04-14 21:20:50 +0100

    feat: solved House Robber problem

---
+++ code/houseRobber.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev2, prev1 = 0, 0
+
+        for num in nums:
+            current = max(prev2 + num, prev1)
+            prev2, prev1 = prev1, current
+
+        return prev1
