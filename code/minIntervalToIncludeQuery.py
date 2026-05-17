e2d907d4fcdeb7260799192a393546864776d479
thobbiz <odelolatojumi@gmail.com>
2026-05-17 23:39:02 +0100

    solved Minimum Interval to Include Each Query problem (LC 1851)

---
+++ code/minIntervalToIncludeQuery.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        intervals.sort()
+        result = [-1] * len(queries)
+        heap = []
+        i = 0
+
+        for q, idx in sorted((q, idx) for idx, q in enumerate(queries)):
+            while i < len(intervals) and intervals[i][0] <= q:
+                l, r = intervals[i]
+                heapq.heappush(heap, (r - l + 1, r))
+                i += 1
+            while heap and heap[0][1] < q:
+                heapq.heappop(heap)
+            if heap:
+                result[idx] = heap[0][0]
+
+        return result
