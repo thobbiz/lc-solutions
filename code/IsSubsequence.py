4b2d8bca69a0baa1067a0168541586108dfdeadd
thobbiz <odelolatojumi@gmail.com>
2026-05-30 21:05:27 +0100

    feat: solved Is Subsequence problem

---
+++ code/IsSubsequence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0
+        for j in range(len(t)):
+            if i == len(s):
+                break
+            if t[j] == s[i]:
+                i += 1
+        return i == len(s)
