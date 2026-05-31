644812358fea03aaf3b14a368f72b6a90d52bd7f
thobbiz <odelolatojumi@gmail.com>
2026-05-31 23:04:37 +0100

    feat: solved Fibonacci Number lc

---
+++ code/fibonacciNumber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        prev, curr = 0, 1
+        for _ in range(2, n + 1):
+            prev, curr = curr, prev + curr
+
+        return curr
