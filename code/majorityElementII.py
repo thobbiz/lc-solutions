603c9df958b5fceb65c947b431c7edaf86b7012a
thobbiz <odelolatojumi@gmail.com>
2026-06-26 23:04:44 +0100

    Update majorityElementII.py

--- code/majorityElementII.py
+++ code/majorityElementII.py
@@ -1,6 +1,16 @@
-def main():
-    print(2 // 3)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        numsMap = {}
+        seen = set()
+        res = []
+        threshold = len(nums) // 3
 
+        for n in nums:
+            if n in seen:
+                continue
+            numsMap[n] = numsMap.get(n, 0) + 1
+            if numsMap[n] > threshold:
+                res.append(n)
+                seen.add(n)
 
-if __name__ == "__main__":
-    main()
+        return res
