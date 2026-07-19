0aea569a152226beefa04144b5e04326b09b4885
thobbiz <odelolatojumi@gmail.com>
2026-07-19 23:33:38 +0100

    feat: solved lc 680 -> Valid Palindrome II

---
+++ code/validPalindromeII.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def isPalindrome(i, j):
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            return True
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return isPalindrome(left + 1, right) or isPalindrome(left, right - 1)
+            left += 1
+            right -= 1
+        return True
