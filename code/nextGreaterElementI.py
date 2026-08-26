07ad788ed64f0750eeefd8de12f09c489e1914a6
thobbiz <odelolatojumi@gmail.com>
2026-08-26 16:25:44 +0100

    feat: solved lc 496 -> Next Greater Element I

---
+++ code/nextGreaterElementI.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashMap = { n:i for i, n in enumerate(nums1)}
+        res = [-1] * len(nums1)
+
+        stack = []
+        for i in range(len(nums2)):
+            cur = nums2[i]
+            while stack and cur > stack[-1]:
+                val = stack.pop()
+                idx = hashMap[val]
+                res[idx] = cur
+            if cur in hashMap:
+                stack.append(cur)
+        return res
