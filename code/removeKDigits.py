844ab379b5125a38d9d747bded16e9cec52107f4
thobbiz <odelolatojumi@gmail.com>
2026-09-04 22:30:21 +0100

    feat: solved lc 402 -> Remove K Digits

---
+++ code/removeKDigits.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+
+        for digit in num:
+            while stack and  k > 0 and stack[-1] > digit:
+                stack.pop()
+                k -= 1
+            stack.append(digit)
+
+        if k > 0:
+            stack = stack[:-k]
+
+        res = "".join(stack).lstrip("0")
+        return res if res else "0"
